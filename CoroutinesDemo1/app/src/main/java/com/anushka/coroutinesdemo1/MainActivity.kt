f577402da681a9dbd619d2ea6a29d9feaaa83ab7
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-06 12:46:45 -0300

    Commit incial do async

--- CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
+++ CoroutinesDemo1/app/src/main/java/com/anushka/coroutinesdemo1/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.anushka.coroutinesdemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
-    lateinit var job: Job
+    private lateinit var userJob: Job
+    private lateinit var locationJob: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,22 +18,45 @@ class MainActivity : AppCompatActivity() {
             tvCount.text = count++.toString()
         }
         btnDownloadUserData.setOnClickListener {
-            job = CoroutineScope(Dispatchers.IO).launch {
+            userJob = CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
+            }
+        }
+        btnDownloadLocationData.setOnClickListener {
+            locationJob = CoroutineScope(Dispatchers.IO).launch {
+                downloadLocationData()
             }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if(::job.isInitialized) {
-            job.cancel()
+        if(::userJob.isInitialized) {
+            userJob.cancel()
+        }
+
+        if(::locationJob.isInitialized) {
+            locationJob.cancel()
         }
     }
 
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for (i in 1..200000) {
-            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+//            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+        }
+    }
+
+    private suspend fun downloadLocationData() {
+        for (i in 1..200000) {
+//            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                tvLocationMessage.text = "Downloading location $i in ${Thread.currentThread().name}"
+            }
+
+            delay(300)
         }
     }
 }
