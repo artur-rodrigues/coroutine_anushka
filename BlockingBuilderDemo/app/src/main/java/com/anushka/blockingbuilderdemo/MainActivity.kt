857ba705623815754f2f994b371a631c052be504
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-06 16:12:24 -0300

    Comit inicial Job

--- BlockingBuilderDemo/app/src/main/java/com/anushka/blockingbuilderdemo/MainActivity.kt
+++ BlockingBuilderDemo/app/src/main/java/com/anushka/blockingbuilderdemo/MainActivity.kt
@@ -11,12 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-           CoroutineScope(Dispatchers.IO).launch {
-               for (i in 1..10) {
-                   delay(100)
-                   Log.i("MyTag", "round $i in ${Thread.currentThread().name}")
-               }
-             }
+        Log.i("MyTag", "this is the start of the onCreate")
 
+        runBlocking(Dispatchers.IO) {
+            for (i in 1..10) {
+                delay(100)
+                Log.i("MyTag", "round $i in ${Thread.currentThread().name}")
+            }
+        }
+
+        Log.i("MyTag", "this is the end 1 of the onCreate")
+        Log.i("MyTag", "this is the end 2 of the onCreate")
     }
 }
---
+++ JobDemo1/app/src/main/java/com/anushka/jobdemo1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.anushka.jobdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
