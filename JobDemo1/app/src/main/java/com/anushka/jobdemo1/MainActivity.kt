73b7c83b80281d309ccb33a7f7158ebb02a205b7
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-06 16:35:26 -0300

    Comit Deferred

--- JobDemo1/app/src/main/java/com/anushka/jobdemo1/MainActivity.kt
+++ JobDemo1/app/src/main/java/com/anushka/jobdemo1/MainActivity.kt
@@ -11,12 +11,17 @@ import kotlinx.coroutines.Dispatchers.Main
 class MainActivity : AppCompatActivity() {
 
     lateinit var job1: Job
+    lateinit var deferred1: Deferred<Int>
+    var returnedValue: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        job1 = CoroutineScope(Main).launch {
+        /**
+         * Funcionamento com Job
+         */
+        /*job1 = CoroutineScope(Main).launch {
             downloadData()
         }
 
@@ -30,6 +35,30 @@ class MainActivity : AppCompatActivity() {
                 job1.isCancelled -> textView.text = "Cancelled"
                 job1.isCompleted -> textView.text = "Completed"
             }
+        }*/
+
+        /**
+         * Funcionamento com Deferred
+         */
+        job1 = CoroutineScope(Main).launch {
+//            downloadData()
+            deferred1 = async {
+                downloadData2()
+            }
+            returnedValue = deferred1.await()
+            log("*** $returnedValue ***")
+        }
+
+        cancelButton.setOnClickListener {
+            deferred1.cancel()
+        }
+
+        statusButton.setOnClickListener {
+            when {
+                deferred1.isActive -> textView.text = "Active"
+                deferred1.isCancelled -> textView.text = "Cancelled"
+                deferred1.isCompleted -> textView.text = "Completed"
+            }
         }
     }
 
@@ -40,6 +69,19 @@ class MainActivity : AppCompatActivity() {
                 log("repeating $it")
             }
         }
+    }
+
+    private suspend fun downloadData2(): Int {
+        var i = 0
+        withContext(IO) {
+            repeat(30) {
+                delay(1000)
+                log("repeating $it")
+                i++
+            }
+        }
+
+        return i
     }
 
     private fun log(message: String) {
