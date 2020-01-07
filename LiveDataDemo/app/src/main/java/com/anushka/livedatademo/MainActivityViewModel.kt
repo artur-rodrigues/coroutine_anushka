277612ecfd5e206cf2b282f8531671b0684596dc
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-07 15:35:39 -0300

    Commit LiveDataDemo comentário.

--- LiveDataDemo/app/src/main/java/com/anushka/livedatademo/MainActivityViewModel.kt
+++ LiveDataDemo/app/src/main/java/com/anushka/livedatademo/MainActivityViewModel.kt
@@ -1,18 +1,19 @@
 package com.anushka.livedatademo
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
-import com.anushka.livedatademo.model.User
 import com.anushka.livedatademo.model.UserRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivityViewModel : ViewModel()  {
 
     private var usersRepository= UserRepository()
+    /**
+     * Para usar o liveData é necessário adicionar a seguinte dependência:
+     * implementation "androidx.lifecycle:lifecycle-livedata-ktx:$arch_version"
+     *
+     * A versão mínima é: 2.2.0-alpha04
+     */
     var users = liveData(Dispatchers.IO) {
         val result = usersRepository.getUsers()
         emit(result)
