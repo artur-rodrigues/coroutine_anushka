ee70edb4e4d7b9aa58242f46f2e28c43ab49a8e7
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-07 15:34:16 -0300

    Commit LiveDataDemo

--- LiveDataDemo/app/src/main/java/com/anushka/livedatademo/MainActivityViewModel.kt
+++ LiveDataDemo/app/src/main/java/com/anushka/livedatademo/MainActivityViewModel.kt
@@ -2,6 +2,7 @@ package com.anushka.livedatademo
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.anushka.livedatademo.model.User
 import com.anushka.livedatademo.model.UserRepository
@@ -12,7 +13,12 @@ import kotlinx.coroutines.withContext
 class MainActivityViewModel : ViewModel()  {
 
     private var usersRepository= UserRepository()
-    var users: MutableLiveData<List<User>> = MutableLiveData()
+    var users = liveData(Dispatchers.IO) {
+        val result = usersRepository.getUsers()
+        emit(result)
+    }
+
+    /*var users: MutableLiveData<List<User>> = MutableLiveData()
 
     fun getUsers() {
         viewModelScope.launch {
@@ -22,5 +28,5 @@ class MainActivityViewModel : ViewModel()  {
             }
             users.value = result
         }
-    }
+    }*/
 }
--- LiveDataDemo/app/src/main/java/com/anushka/livedatademo/model/UserRepository.kt
+++ LiveDataDemo/app/src/main/java/com/anushka/livedatademo/model/UserRepository.kt
@@ -6,12 +6,11 @@ class UserRepository {
 
     suspend fun getUsers(): List<User> {
         delay(8000)
-        val users: List<User> = listOf(
+        return listOf(
             User(1, "Sam"),
             User(2, "Taro"),
             User(3, "Jane"),
             User(4, "Amy")
         )
-        return users
     }
 }
