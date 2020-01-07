e5f8bf519074407af0482f465ddea62c6ae34307
artur.rodrigues <artur.rodrigues@tivit.com>
2020-01-07 09:38:26 -0300

    Commit inicial join

---
+++ JoinDemo/app/src/main/java/com/anushka/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.anushka.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.i("MyTag","Coroutine luanched from ${Thread.currentThread().name}")
+
+        }
+    }
+}
